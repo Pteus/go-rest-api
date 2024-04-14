d34728478bdaf01e69b06b52e2877ba1915957a5
Pedro Mateus <pedropmateus91@gmail.com>
2024-04-14 23:07:21 +0100

    User

---
+++ routes/users.go
@@ -0,0 +1,43 @@
+package routes
+
+import (
+	"net/http"
+
+	"example.com/rest-api/models"
+	"github.com/gin-gonic/gin"
+)
+
+func signup(context *gin.Context) {
+	var user models.User
+	err := context.ShouldBindJSON(&user)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse user"})
+		return
+	}
+
+	err = user.Save()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not save user"})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"message": "User created sucessfully"})
+}
+
+func login(context *gin.Context) {
+	var user models.User
+	err := context.ShouldBindJSON(&user)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse user"})
+		return
+	}
+
+	err = user.ValidateCredentials()
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": err.Error()})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "logged in"})
+
+}
